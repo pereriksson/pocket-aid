eae1ccfd9f3e75ac64ce1fefa522dab64668fba4
Per Eriksson <per@interapp.se>
2022-06-17 14:50:00 +0200

    expenses view

--- app/src/main/java/se/umu/cs/peer0019/pocketaid/ReportActivity.kt
+++ app/src/main/java/se/umu/cs/peer0019/pocketaid/ReportActivity.kt
@@ -8,26 +8,46 @@ import com.github.aachartmodel.aainfographics.aaoptionsmodel.*
 class ReportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(CanadaChart(this))
-        // TODO: Använd en stacked bar chart för vyn
-        // https://github.com/AAChartModel/AAChartCore-Kotlin
-        // TODO: Sätt legend color till vit, dölj datalabels för att få en större pie chart
 
+        // TODO: Lägg lite tid på att hitta ett snyggt färgschema
+        // TODO: Knapp på första sidan "Lägg in utgift"
+        // TODO: Inställningar (lägga till kategorier, beloppspärr etc)
+        // TODO: Gör en YouTube-video
+        // TODO: Det ska gå att göra pie charten till en cirkel!
+        // TODO: Gör en egen snygg klass av detta
+        // TODO: activities:
+        // TODO: HomeActivity
+        // TODO: AddExpenseActivity
+        // TODO: SettingsActivity
+        // TODO: ReportActivity
+        val plotOptions = AAPlotOptions()
+            .series(AASeries()
+                .borderColor("transparent")
+            )
+        // Gör till en view?
         val aaChartModel : AAChartModel = AAChartModel()
             .chartType(AAChartType.Pie)
-            .titleStyle(
-                AAStyle()
-                    .color("#ffffff")
-                    .fontSize(20)
-            )
-            .backgroundColor("#2C2F31")
+            .backgroundColor("transparent")
             .dataLabelsEnabled(true)
+            .colorsTheme(
+                arrayOf(
+                    // TODO: Create strings for these
+                    // TODO: Make sure we have enough colors as categories are defined by the user
+                    "#7EFEBF",
+                    "#0ACAF3",
+                    "#FF107C",
+                    "#FFBF68",
+                    "#ED71FE"
+                )
+            )
             .series(arrayOf(
                 AASeriesElement()
                     .data(arrayOf(
                         arrayOf("Bio", 10),
                         arrayOf("Elektronik", 20),
-                        arrayOf("Mat", 80)
+                        arrayOf("Mat", 80),
+                        arrayOf("Film", 30),
+                        arrayOf("Resa", 40)
                     ))
                     .dataLabels(
                         AADataLabels()
@@ -48,10 +68,10 @@ class ReportActivity : AppCompatActivity() {
                     )
                     .itemMarginTop(16.toFloat())
             )
+            //.plotOptions(plotOptions)   //legenden försvinner
 
         setContentView(R.layout.activity_report)
         val aaChartView = findViewById<AAChartView>(R.id.aa_chart_view)
-        //aaChartView.aa_drawChartWithChartModel(aaChartModel)
         aaChartView.aa_drawChartWithChartOptions(aaChartOptions)
     }
 }
---
+++ app/src/main/java/se/umu/cs/peer0019/pocketaid/SettingsActivity.kt
@@ -0,0 +1,7 @@
+package se.umu.cs.peer0019.pocketaid
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+}
